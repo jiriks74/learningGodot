6a1163529738b5c3eaea218f3415fb9ad81461f4
jiriks74 <jiri.stefka@atlas.cz>
2021-10-20 20:34:23 +0200

    Release v1.0

--- Pong4Two/Main.cs
+++ Pong4Two/Main.cs
@@ -3,32 +3,111 @@ using System;
 
 public class Main : Node
 {
-    [Export]
-    // Constant for ball speed (in pixels/second)
-    const int INITIAL_BALL_SPEED = 80;
 
+    // Constant for ball speed (in pixels/second)
     [Export]
+    int INITIAL_ballSpeed = 80;
+
     // Constant for pads speed
-    const int PAD_SPEED = 150;
+    [Export]
+    int PAD_SPEED = 150;
 
     // Speed of the ball (also in pixels/second)
-    int ball_speed = INITIAL_BALL_SPEED;
+    float ballSpeed;
+    Vector2 ballPosition;
 
     Vector2 _screenSize;
     //Shape2D padSize;
     Vector2 direction = new Vector2(1.0f, 0.0f);
 
+    float lastDelta;
+
+    static float NextFloat()
+    {
+        Random rand = new Random();
+        float randomFloat = (float)rand.NextDouble();
+        return randomFloat;
+    }
+
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
         _screenSize = GetViewport().Size;
+        ballSpeed = INITIAL_ballSpeed;
+        GetNode<Area2D>("Ball").Position += direction;
         //padSize = GetNode<CollisionShape2D>("LeftPaddle/CollisionShape2D").Shape;
-        //SetProcess(true);
+        SetProcess(true);
     }
 
-    //  // Called every frame. 'delta' is the elapsed time since the previous frame.
+    // public override void _Process(float delta)
+    // {
+    //     GetNode<Area2D>("Ball").Position += direction;
+    // }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(float delta)
     {
-        //Position2D ball = GetNode<Ball>("Ball").Position;
+        // Get ball position
+        ballPosition = GetNode<Area2D>("Ball").Position;
+
+        // Check if the ball hits top or bottom of the screen
+        if ((ballPosition.y < 0 && direction.y < 0) || (ballPosition.y > _screenSize.y && direction.y > 0))
+        {
+            direction.y = -direction.y; // Invert direction
+        }
+
+        // Check if ball area entered one of pad's area and if so, redirect it on x axis and set random direction on y axis
+        if ((GetNode<Area2D>("LeftPaddle").OverlapsArea(GetNode<Area2D>("Ball"))) || (GetNode<Area2D>("RightPaddle").OverlapsArea(GetNode<Area2D>("Ball"))))
+        {
+            direction.x = -direction.x; // Invert x axis direction
+            direction.y = NextFloat() * 2.0f - 1f; // Get random float for y axis
+            direction = direction.Normalized(); // Normalize the direction
+            ballSpeed *= 1.1f; // Multiply the speed (so it get's harder over time)
+            GetNode<CollisionShape2D>("Ball/CollisionShape2D").SetDeferred("disabled", true);
+        }
+
+        // If ball get's outside of screen on x axis (goal), teleport it to the middle of the screen
+        if (ballPosition.x < 0 || ballPosition.x > _screenSize.x)
+        {
+            ballPosition = _screenSize * 0.5f; // Set the ball's position to the middle of the screen
+            ballSpeed = INITIAL_ballSpeed; // Set the speed to initial speed
+            direction = new Vector2(-1, 0); // Set default direction
+        }
+
+        // Integrate new ball position
+        ballPosition += direction * ballSpeed * delta;
+
+        // Update ball's position
+        GetNode<Area2D>("Ball").Position = ballPosition;
+
+        // Move left pad
+        Vector2 leftPosition = GetNode<Area2D>("LeftPaddle").Position;
+
+        if (leftPosition.y > 0 && Input.IsActionPressed("LeftUp"))
+        {
+            leftPosition.y += -PAD_SPEED * delta;
+        }
+        if (leftPosition.y < _screenSize.y && Input.IsActionPressed("LeftDown"))
+        {
+            leftPosition.y += PAD_SPEED * delta;
+        }
+
+        GetNode<Area2D>("LeftPaddle").Position = leftPosition;
+
+        // Move right pad
+        Vector2 rightPosition = GetNode<Area2D>("RightPaddle").Position;
+
+        if (rightPosition.y > 0 && Input.IsActionPressed("RightUp"))
+        {
+            rightPosition.y += -PAD_SPEED * delta;
+        }
+        if (rightPosition.y < _screenSize.y && Input.IsActionPressed("RightDown"))
+        {
+            rightPosition.y += PAD_SPEED * delta;
+        }
+
+
+        GetNode<Area2D>("RightPaddle").Position = rightPosition;
+        GetNode<CollisionShape2D>("Ball/CollisionShape2D").Disabled = false;
     }
 }
